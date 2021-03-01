7b9ff1d2ef69a9ad361a611242251d070be0cd6e
scala62 <ning.chen.spark@gmail.com>
2021-02-28 23:16:38 -0500

    Feb28th

--- Leetcode236LCA.java
+++ Leetcode236LCA.java
@@ -6,6 +6,7 @@ class Leetcode236LCA {
         if (root == p || root == q) {
             return root;
         }
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         if (left != null && right != null) {
